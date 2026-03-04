680e55882ef49d074199c1c7fed3e87ceea0da4c
Alexandr_Black <malcev2008sahar@gmail.com>
2026-03-04 17:51:06 +0500

    z1

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: 'birthday-present', // Замени на название твоего репозитория!
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
